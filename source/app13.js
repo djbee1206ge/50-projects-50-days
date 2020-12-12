01c2362ec64fc074cf02ee7f0773056dba6030d0
djbee1206ge <33894403+djbee1206ge@users.noreply.github.com>
2020-12-12 07:11:30 +0100

    Add day 13

---
+++ source/app13.js
@@ -0,0 +1,64 @@
+const textarea = document.getElementById('textarea');
+const tagsEl = document.getElementById('tags');
+
+textarea.focus();
+textarea.addEventListener('keyup', (e) => {
+	createTags(e.target.value);
+
+	if (e.key === 'Enter') {
+		setTimeout(() => {
+			e.target.value = '';
+		}, 50);
+		randomChoice();
+	}
+});
+
+function createTags(input) {
+	const tags = input
+		.split(',')
+		.filter((tag) => tag.trim() !== '')
+		.map((tag) => tag.trim());
+	tagsEl.innerHTML = '';
+
+	tags.forEach((tag) => {
+		const tagEl = document.createElement('span');
+		tagEl.classList.add('tag');
+		tagEl.innerText = tag;
+		tagsEl.appendChild(tagEl);
+	});
+}
+
+function randomChoice() {
+	const times = 30;
+
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+
+		highlightTag(randomTag);
+
+		setTimeout(() => {
+			unHighlightTag(randomTag);
+		}, 100);
+	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+		const randomTag = pickRandomTag();
+		setTimeout(() => {
+			highlightTag(randomTag);
+			1, 2, 3, 4, 5;
+		}, 100);
+	}, times * 100);
+}
+
+function pickRandomTag() {
+	const tags = document.querySelectorAll('.tag');
+	return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+	tag.classList.add('highlight');
+}
+function unHighlightTag(tag) {
+	tag.classList.remove('highlight');
+}
