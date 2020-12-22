a43010208ba996337b05387da6d18149e7920bb5
djbee1206ge <33894403+djbee1206ge@users.noreply.github.com>
2020-12-22 09:42:45 +0100

    Day 24

---
+++ source/app24.js
@@ -0,0 +1,25 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const ProfileName = document.getElementById('name')
+const date = document.getElementById('date')
+const animated_bgs = document.querySelectorAll('animated-bg')
+const animated_bg_txts = document.querySelectorAll('animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML = '<img src="../assets/img/coder.jpg" alt="Photo by Shahadat Rahman on Unsplash">'
+  title.innerHTML = 'Front-end Developer'
+  excerpt.innerHTML = 'I design and code beautifully simple things, and I love what I do.'
+  profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
+  ProfileName.innerHTML = 'John Doe'
+  date.innerHTML = 'Dec 25, 2020'
+
+  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+  animated_bg_txts.forEach(txt => txt.classList.remove('animated-bg-text'))
+
+
+
+}
