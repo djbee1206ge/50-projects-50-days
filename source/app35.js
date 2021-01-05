d6286a09409f2e9f9d0629fc9b6ee5ff069b9a15
djbee1206ge <33894403+djbee1206ge@users.noreply.github.com>
2021-01-05 05:43:58 +0100

    day 35

---
+++ source/app35.js
@@ -0,0 +1,38 @@
+const imgs = document.querySelector('#imgs')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+let interval = setInterval(run, 2000)
+
+function run() {
+  idx++
+  changeImage()
+}
+
+function changeImage() {
+  if (idx > img.length - 1) {
+    idx = 0
+  } else if (idx < 0) {
+    idx = img.length - 1
+  }
+  imgs.style.transform = `translateX(${-idx * 500}px)`
+
+}
+
+function resetInterval() {
+  clearInterval(interval)
+  interval = setInterval(run, 2000)
+}
+right.addEventListener('click', () => {
+  idx++
+  changeImage()
+  resetInterval()
+})
+left.addEventListener('click', () => {
+  idx--
+  changeImage()
+  resetInterval()
+})
+
